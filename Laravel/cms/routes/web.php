21c78634e42101bd99868131615bad5cf2d7cedf
Alptekin <alptekin.ocakdan@gmail.com>
2023-01-24 17:40:49 +0300

    Entering the laravel

---
+++ Laravel/cms/routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return "Hi Aboout Page";
+});
+Route::get('/contact', function () {
+    return "Hi Contact Page";
+});
+
+Route::get('/post/{id}/{name}', function ($id, $name) {
+    return "This is post number " . $id . " " . $name;
+});
+
+Route::get('admin/posts/example', array('as' => 'admin.home', function () {
+    $url = route('admin.home');
+    return "this url is " . $url;
+}));
+
+Route::get('/posts',[PostsController::class,'index']);
